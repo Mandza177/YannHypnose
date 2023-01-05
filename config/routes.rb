e4ac11560c91f84c733d25e9dfb4e8b4c0d2ec7f
Yann Lecocq <mandza@hotmail.fr>
2023-01-05 16:20:16 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "/contact", to: "pages#contact"
+  get "/biographie", to: "pages#biographie"
+  get "/hypnose", to: "pages#hypnose"
+  get "/tarif", to: "pages#tarif"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
